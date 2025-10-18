e79257e2c0a04659bc6db1c9a0a7f65a4def15bd
Neyo <naledihloni14@gmail.com>
2025-10-18 12:38:19 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -9,3 +9,8 @@ except ImportError:
 DB = "alx_book_store"
 
 SHOW TABLE books;
+[(
+  "INFORMATION_SCHEMA.COLUMNS", "SELECT", "COLUMN_NAME",
+  "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'",
+  "TABLE_NAME = 'Books'"
+  )]
