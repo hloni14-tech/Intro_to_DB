b190f5d23ed8b170a96ef4d1332d3b6a92e53499
Neyo <naledihloni14@gmail.com>
2025-10-18 13:32:45 +0200

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -15,10 +15,10 @@ VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness  Ave.");
 INSERT INTO customer (customer_id, customer_name,
 email, address)
   VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', 
-'125 Happiness Ave.');
+"125 Happiness  Ave.");
               
 INSERT INTO customer (customer_id, customer_name,
 email, address)
   VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com',
-'126 Happiness Ave.');
+"126 Happiness  Ave.");
 
