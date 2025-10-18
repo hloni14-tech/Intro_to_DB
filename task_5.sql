459c04ac326e8a7ff485dc9b43f8633def516cf4
Neyo <naledihloni14@gmail.com>
2025-10-18 12:52:31 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -11,4 +11,4 @@ DB = "alx_book_store"
 INSERT INTO customer (customer_id,customer_name,
 email,address)
 VALUES (1, 'Cole' 'Baidoo', 'cbaidoo@sandtech.com',
-123 'Happiness Ave.');
+'123 Happiness Ave.');
